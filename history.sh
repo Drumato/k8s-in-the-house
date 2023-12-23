0f9f1bdd991543f969b8e073be18f42b534e8653
Drumato <drumato43@gmail.com>
2023-12-23 15:13:38 +0900

    add simple1 manifest
    Signed-off-by: Drumato <drumato43@gmail.com>

--- history.sh
+++ history.sh
@@ -2,4 +2,9 @@ kind create cluster --config kind-config.yaml
 
 bash ./scripts/apply-gateways-api.sh
 helmfile sync -f helmfiles/helmfile.yaml
+
 kubectl apply -f manifests/metallb.yaml
+
+cilium connectivity test
+
+kubectl apply -f manifests/argocd-ingress.yaml
